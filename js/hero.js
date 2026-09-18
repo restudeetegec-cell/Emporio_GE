af2ada39a35d17b60129c724264fbd46de604d6a
VictorGBR04035 <victorgbr0403@gmail.com>
2026-09-18 17:38:28 -0300

    feat(home): restructure home page and add new modules
    - HTML: update index.html structure
    - CSS: create header.css, hero.css and adjust main.css
    - JS: add hero.js for interactivity
    - Assets: include new project images

--- js/hero.js
+++ js/hero.js
@@ -1,24 +1,26 @@
-const slider = document.querySelector('.hero-slider');
+const slides = document.querySelectorAll('.hero-slide');
 
 let currentIndex = 0;
 const intervalTime = 5000; // Muda a cada 5 segundos (5000 milissegundos)
 
 function nextSlide() {
     // Remove a classe 'active' do slide atual
-    slider[currentIndex].classList.remove('active');
+    slides[currentIndex].classList.remove('active');
 
     // 1 2 3
     // 1 % 3
     // 2 % 3
     // 4 % 3 => 1
     // Incrementa o índice para o próximo slide
-    currentIndex = (currentIndex + 1) % slider.length;
+    currentIndex = (currentIndex + 1) % slides.length;
 
     // Adiciona a classe 'active' ao próximo slide
-    slider[currentIndex].classList.add('active');
+    slides[currentIndex].classList.add('active');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     //Inicia o loop de slides
+if (slides.length > 0) {
     setInterval(nextSlide, intervalTime);
+    }
 });
