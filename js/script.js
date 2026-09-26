67cf6da34150655816f9b1ac49bfdea71124b121
VictorGBR04035 <victorgbr0403@gmail.com>
2026-09-26 11:03:42 -0300

    feat(catalog): improve product cards and search

--- js/script.js
+++ js/script.js
@@ -19,3 +19,113 @@ function moverCarrossel()  {
 }
 // Faz moverw a cada 20 milissegundo
 setInterval(moverCarrossel, 20); 
+
+
+// CATÁLOGO
+const produtos = [
+    {
+        nome: "Atum",
+        preco: "R$ 10,00",
+        imagem: "images/atum.jpg"
+    },
+
+    {
+        nome: "Corvina",
+        preco: "R$ 35,00",
+        imagem: "images/Corvina.webp"
+    },
+
+    {
+        nome: "Fileti",
+        preco: "R$ 24,00",
+        imagem: "images/fileti.webp"
+    },
+
+    {
+        nome: "Tilápia",
+        preco: "R$25.00",
+        imagem: "images/tilapia.jpg"
+    },
+
+    {
+        nome: "Sardinha",
+        preco: "R$25.00",
+        imagem: "images/Sardinha.jpg"
+    }
+]
+
+const listaProdutos = document.getElementById("listaProdutos");
+
+function mostrarProdutos(lista) {
+    listaProdutos.innerHTML = "";
+    lista.forEach(produto => {
+
+        const card = document.createElement("article");
+        card.classList.add("produto");
+
+        card.innerHTML = `
+            <div class="produto-imagem">
+
+                <img
+                    src="${produto.imagem}"
+                    alt="${produto.nome}"
+                >
+
+            </div>
+
+            <h3>${produto.nome}</h3>
+
+            <p class="produto-preco">
+                ${produto.preco}
+            </p>
+        `;
+
+        listaProdutos.appendChild(card);
+        
+    });
+}
+
+mostrarProdutos(produtos);
+
+const abrirPesquisa = document.getElementById("abrirPesquisa");
+const fecharPesquisa = document.getElementById("fecharPesquisa");
+const searchOverlay = document.getElementById("searchOverlay");
+const campoPesquisa = document.getElementById("campoPesquisa");
+
+abrirPesquisa.addEventListener("click", () => {
+
+    searchOverlay.classList.add("ativo");
+
+    campoPesquisa.focus();
+
+});
+
+fecharPesquisa.addEventListener("click", () => {
+
+    searchOverlay.classList.remove("ativo");
+
+    campoPesquisa.value = "";
+
+    mostrarProdutos(produtos);
+
+});
+
+campoPesquisa.addEventListener("input", () => {
+
+    const texto = campoPesquisa.value
+        .toLowerCase()
+        .trim();
+
+
+    const resultados = produtos.filter(produto =>
+
+        produto.nome
+            .toLowerCase()
+            .includes(texto)
+
+    );
+
+
+    mostrarProdutos(resultados);
+
+});
